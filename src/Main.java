8716c21b4f69b3cf4b283e46742f86e3ee974918
Al_by <edu.123gts@gmail.com>
2024-11-05 21:50:21 -0500

    readme test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 5;
-
-        int mayor = num1 > num2? num1 : num2;
-
-        System.out.println(mayor);
+        System.out.println("Hello World");
     }
 }
