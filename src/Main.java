e37db19e5e7c6bd761f6cd06028f936e0335d21b
Al_by <edu.123gts@gmail.com>
2024-11-06 21:59:43 -0500

    Ejercicios 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+//    =====================================================
+//    1. Average
+//    public static void main(String[] args) {
+//        int[] nums= {8,2,3};
+//        int sum = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            sum += nums[i];
+//        }
+//        System.out.println((double) sum/nums.length);
+//    }
+//    =====================================================
+//    2. Print pattern
+//    public static void main(String[] args) {
+//        printPattern('-', 18,8);
+//    }
+//
+//    public static void printPattern(char value, int patternWidth, int patternHeight) {
+//        for (int i = 0; i < patternHeight; i++) {
+//            for (int j = 0; j < patternWidth; j++) {
+//                System.out.print(value);
+//            }
+//            System.out.println("");
+//        }
+//    }
+//    =====================================================
+//  3. Find value in an array
+//    public static void main(String[] args) {
+//        int[] data ={1,2,3};
+//        findValue(data, 3);
+//    }
+//
+//    public static void findValue(int[] list,int value){
+//        for(int i=0;i<list.length;i++){
+//            if(list[i]==value){
+//                System.out.println("The value " + value + " is found");
+//            }
+//            if(i==list.length-1 && list[i] != value){
+//                System.out.println("The value " + value + " is not found");
+//            }
+//        }
+//    }
+//    4. Find the index of a value in a array
+//    public static void main(String[] args) {
+//        int[] data ={1,2,3};
+//        findIndexOfValue(data, 5);
+//    }
+//
+//    public static void findIndexOfValue(int[] list,int value){
+//        for(int i=0;i<list.length;i++){
+//            System.out.println("[" + list[i] + "] --> position:" + i);
+//            if(list[i]==value){
+//                System.out.println("The value " + value + " has an index of " + i);
+//            }
+//            if(i==list.length-1 && list[i] != value){
+//                System.out.println("The value " + value + " is not found");
+//            }
+//        }
+//    }
 }
